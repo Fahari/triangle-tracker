95eeb3f002f7e7874f7b0882a8084c67bf1c00e2
Fahari <karonjekevin67@gmail.com>
2018-12-02 14:03:00 +0300

    add JScript code that initializes variables and an array that will be used to store input variables.

---
+++ js/brain.js
@@ -0,0 +1,8 @@
+function brain() {
+  var sides = new Array;                                 //create an array called "sides" that will store variables containing user input values.
+  var inputA = document.getElementById('SideA').value;  //create variables that will store user input values.
+  var inputA = document.getElementById('SideB').value;
+  var inputA = document.getElementById('SideC').value;
+  sides.push(inputA,inputB,inputC);                     //push variables containing user input in the empty array "sides".
+
+}
